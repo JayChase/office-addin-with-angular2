a90a8e5ffa4adb8186197ff9e73c0ecaa3812d29
JayChase <JonathanChase@outlook.com>
2016-08-17 09:21:59 +0900

    Moved back to office init

--- OfficeAddInWithAngular2Web/addin/main.ts
+++ OfficeAddInWithAngular2Web/addin/main.ts
@@ -9,10 +9,10 @@ import {enableProdMode} from '@angular/core';
 enableProdMode();
 
 //use this to run in browser for dev
-platformBrowserDynamic().bootstrapModule(AppModule);
+//platformBrowserDynamic().bootstrapModule(AppModule);
 
 //bootstrap with Office.js for in Office
-// Office.initialize = () => {
-//     console.log("Office init: bootstrapping Angular2");
-//     platformBrowserDynamic().bootstrapModule(AppModule);
-// }
+ Office.initialize = () => {
+     console.log("Office init: bootstrapping Angular2");
+     platformBrowserDynamic().bootstrapModule(AppModule);
+ }
